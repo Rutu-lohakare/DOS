da6dd0eaf2c1445c41e6d836e4858d469e1ac741
rutuja lohakare <53614295+Rutu-lohakare@users.noreply.github.com>
2021-03-16 17:04:32 +0530

    added assignment task files

---
+++ test.sql
@@ -0,0 +1,7 @@
+create table studentData (Name TEXT, Roll_no INT, Address TEXT, Mobile_Number TEXT, PAN_Number TEXT);
+
+INSERT into studentData (Name,Roll_no,Address,Mobile_Number,PAN_Number) values ('xyz',11210,'pune','7782903546','8745429611544');
+INSERT into studentData (Name,Roll_no,Address,Mobile_Number,PAN_Number) values ('abc',10110,'sonai','9204983425','1295355256118');
+INSERT into studentData (Name,Roll_no,Address,Mobile_Number,PAN_Number) values ('klm',29310,'thane','8839201843','7343290189014');
+INSERT into studentData (Name,Roll_no,Address,Mobile_Number,PAN_Number) values ('uvx',12310,'mumbai','8123983545','4578302891232');
+INSERT into studentData (Name,Roll_no,Address,Mobile_Number,PAN_Number) values ('lmn',18127,'vakad','96042873543','1283645329323');
